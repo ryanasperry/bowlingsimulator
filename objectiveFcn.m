054d8696dd805e3d2b9cc6ee6f660d2d2a5b019e
Ryan Sperry <ryanasperry@gmail.com>
2020-03-17 13:22:50 -0600

    changed view option

--- objectiveFcn.m
+++ objectiveFcn.m
@@ -4,7 +4,7 @@ direction  = x(1)*30;
 position = x(2)*.52705;
 velocity(1) = speed*sin(direction*pi/180);
 velocity(2) = speed*cos(direction*pi/180);
-world = World([position, -.25], velocity, 0); %change to 1 to show simulation
+world = World([position, -.25], velocity, 1); %change to 1 to show simulation
 
 for i = 1:100
     world.update();
