d9af053f8af2939d73d9f56cdf5b7d2fba259e51
Ryan Sperry <ryanasperry@gmail.com>
2020-04-06 09:16:36 -0600

    modified inputs

--- objectiveFcn.m
+++ objectiveFcn.m
@@ -6,10 +6,10 @@ mass = x(3)*10;
 velocity(1) = speed*sin(direction*pi/180);
 velocity(2) = speed*cos(direction*pi/180);
 world = World([position, -.25], velocity, mass, 0); %change to 1 to show simulation
-
-for i = 1:100
+numberofSteps = 50+(1-x(4))*300;
+for i = 1:numberofSteps
     world.update();
 end
-J = (mass*speed^2)/world.momentum(length(world.momentum));
+J = (mass*speed)/world.momentum(length(world.momentum))^2;
 d = world.displacement;
 end
